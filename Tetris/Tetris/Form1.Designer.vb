fd4caf0c9eed4365d9acf8763cc3360641c416b9
Jalexander <jhale767@learner.dvusd.org>
2024-09-20 12:38:51 -0700

    First version of game

---
+++ Tetris/Tetris/Form1.Designer.vb
@@ -0,0 +1,107 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        components = New ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Avatar = New PictureBox()
+        Timer1 = New Timer(components)
+        Enemy = New PictureBox()
+        PictureBox3 = New PictureBox()
+        Enemy2 = New PictureBox()
+        CType(Avatar, ComponentModel.ISupportInitialize).BeginInit()
+        CType(Enemy, ComponentModel.ISupportInitialize).BeginInit()
+        CType(PictureBox3, ComponentModel.ISupportInitialize).BeginInit()
+        CType(Enemy2, ComponentModel.ISupportInitialize).BeginInit()
+        SuspendLayout()
+        ' 
+        ' Avatar
+        ' 
+        Avatar.Image = CType(resources.GetObject("Avatar.Image"), Image)
+        Avatar.Location = New Point(158, 103)
+        Avatar.Name = "Avatar"
+        Avatar.Size = New Size(124, 201)
+        Avatar.SizeMode = PictureBoxSizeMode.Zoom
+        Avatar.TabIndex = 0
+        Avatar.TabStop = False
+        ' 
+        ' Timer1
+        ' 
+        Timer1.Enabled = True
+        Timer1.Interval = 150
+        ' 
+        ' Enemy
+        ' 
+        Enemy.Image = CType(resources.GetObject("Enemy.Image"), Image)
+        Enemy.Location = New Point(365, 103)
+        Enemy.Name = "Enemy"
+        Enemy.Size = New Size(95, 95)
+        Enemy.SizeMode = PictureBoxSizeMode.Zoom
+        Enemy.TabIndex = 1
+        Enemy.TabStop = False
+        ' 
+        ' PictureBox3
+        ' 
+        PictureBox3.Image = CType(resources.GetObject("PictureBox3.Image"), Image)
+        PictureBox3.Location = New Point(518, 219)
+        PictureBox3.Name = "PictureBox3"
+        PictureBox3.Size = New Size(108, 95)
+        PictureBox3.SizeMode = PictureBoxSizeMode.Zoom
+        PictureBox3.TabIndex = 2
+        PictureBox3.TabStop = False
+        ' 
+        ' Enemy2
+        ' 
+        Enemy2.Image = CType(resources.GetObject("Enemy2.Image"), Image)
+        Enemy2.Location = New Point(365, 242)
+        Enemy2.Name = "Enemy2"
+        Enemy2.Size = New Size(95, 95)
+        Enemy2.SizeMode = PictureBoxSizeMode.Zoom
+        Enemy2.TabIndex = 3
+        Enemy2.TabStop = False
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(8F, 20F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(811, 858)
+        Controls.Add(Enemy2)
+        Controls.Add(PictureBox3)
+        Controls.Add(Enemy)
+        Controls.Add(Avatar)
+        Name = "Form1"
+        Text = "Form1"
+        CType(Avatar, ComponentModel.ISupportInitialize).EndInit()
+        CType(Enemy, ComponentModel.ISupportInitialize).EndInit()
+        CType(PictureBox3, ComponentModel.ISupportInitialize).EndInit()
+        CType(Enemy2, ComponentModel.ISupportInitialize).EndInit()
+        ResumeLayout(False)
+    End Sub
+
+    Friend WithEvents Avatar As PictureBox
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents Enemy As PictureBox
+    Friend WithEvents PictureBox3 As PictureBox
+    Friend WithEvents Enemy2 As PictureBox
+
+End Class
---
+++ Tetris/Tetris/Form1.vb
@@ -0,0 +1,45 @@
+﻿Public Class Form1
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        Follow(Enemy, Avatar, 15) 'GTA5 source code moment'
+        Follow(Enemy2, Avatar, 10)
+    End Sub
+    Sub Follow(E As PictureBox, A As PictureBox, S As Integer)
+        If E.Location.Y > A.Location.Y Then
+            Move(E, 0, -S)
+        Else
+            Move(E, 0, S)
+        End If
+        If E.Location.X > A.Location.X Then
+            Move(E, -S, 0)
+        Else 'the imposter from among us'
+            Move(E, S, 0) 'when the imposter is sus'
+        End If
+    End Sub
+    Private Sub Form1_KeyDown(sender As Object, e As KeyEventArgs) Handles MyBase.KeyDown
+        If e.KeyCode = Keys.Left Or e.KeyCode = Keys.A Then
+            Move(Avatar, -15, 0)
+        End If
+        If e.KeyCode = Keys.Right Or e.KeyCode = Keys.D Then
+            Move(Avatar, 15, 0)
+        End If
+        If e.KeyCode = Keys.Down Or e.KeyCode = Keys.S Then
+            Move(Avatar, 0, 15)
+        End If
+        If e.KeyCode = Keys.Up Or e.KeyCode = Keys.W Then
+            Move(Avatar, 0, -15)
+        End If
+        If e.KeyCode = Keys.Space Then
+            Avatar.Image.RotateFlip(RotateFlipType.Rotate90FlipNone)
+        End If
+        Avatar.Refresh()
+
+    End Sub
+
+    Sub Move(p As PictureBox, xdir As Integer, ydir As Integer)
+        p.Location += New Point(xdir, ydir)
+        If p.Bounds.IntersectsWith(PictureBox3.Bounds) Then
+            PictureBox3.Visible = False
+        End If
+    End Sub
+
+End Class
